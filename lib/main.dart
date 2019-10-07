0cf17fe3c0d28e07835aa1577198d2495eceb17e
Ruben <lukesruben@gmail.com>
2019-10-07 15:55:43 +0200

    added note detail widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:note_reminder/note_detail.dart';
 import 'package:note_reminder/screens/note_list.dart';
 
 void main() => runApp(NoteReminder());
@@ -13,7 +14,7 @@ class NoteReminder extends StatelessWidget {
         primarySwatch: Colors.deepOrange
       ),
       home: Center(
-        child: NoteList()
+        child: NoteDetail()
       ),
     );
   }
--- lib/note_detail.dart
+++ lib/note_detail.dart
@@ -1,1 +1,108 @@
 import 'package:flutter/material.dart';
+
+class NoteDetail extends StatefulWidget {
+  @override
+  _NoteDetailState createState() => _NoteDetailState();
+}
+
+class _NoteDetailState extends State<NoteDetail> {
+  static var _priorities = ['Hight', 'Low'];
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    TextStyle txtStyle = Theme.of(context).textTheme.title;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Edit Note"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 15.0, left: 10.0, right: 10.0),
+        child: ListView(
+          children: <Widget>[
+            ListTile(
+              title: DropdownButton(
+                items: _priorities.map((String p) {
+                  return DropdownMenuItem<String>(
+                    child: Text(p),
+                    value: p,
+                  );
+                }).toList(),
+                style: txtStyle,
+                value: 'Low',
+                onChanged: (value) {
+                  setState(() {});
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 15, bottom: 15),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  labelText: "Title",
+                  labelStyle: txtStyle,
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0)),
+                ),
+                controller: titleController,
+                style: txtStyle,
+                onChanged: (value) {},
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 15, bottom: 15),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  labelText: "Description",
+                  labelStyle: txtStyle,
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0)),
+                ),
+                controller: descriptionController,
+                style: txtStyle,
+                onChanged: (value) {},
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 15, bottom: 15),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: RaisedButton(
+                      color: Theme.of(context).primaryColorDark,
+                      textColor: Theme.of(context).primaryColorLight,
+                      child: Text(
+                        "Save",
+                        textScaleFactor: 1.5,
+                      ),
+                      onPressed: () {
+                        setState(() {});
+                      },
+                    ),
+                  ),
+                  Container(
+                    width: 5.0,
+                  ),
+                  Expanded(
+                    child: RaisedButton(
+                      color: Theme.of(context).primaryColorDark,
+                      textColor: Theme.of(context).primaryColorLight,
+                      child: Text(
+                        "Delete",
+                        textScaleFactor: 1.5,
+                      ),
+                      onPressed: () {
+                        setState(() {});
+                      },
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
