744fd44c83bfd2d98468afa48ec5e238c3fbfd8c
Ruben <lukesruben@gmail.com>
2019-10-07 14:58:35 +0200

    added note list view

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:note_reminder/screens/note_list.dart';
+
+void main() => runApp(NoteReminder());
+
+class NoteReminder extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Note Reminder",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange
+      ),
+      home: Center(
+        child: NoteList()
+      ),
+    );
+  }
+}
---
+++ lib/screens/note_list.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class NoteList extends StatefulWidget {
+  @override
+  _NoteListState createState() => _NoteListState();
+}
+
+class _NoteListState extends State<NoteList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Notes'),
+      ),
+      body: getNoteListView(),
+    );
+  }
+
+  getNoteListView(){
+    return Center(child: Text("notes..."),);
+  }
+}
